5e2b8b01a67fa7bd0386f7c9a68a3254d86cc5e8
Milan Santosi <milan.santosi@gmail.com>
2024-09-07 14:37:21 +0200

    reset master to scratch

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-;;; Directory Local Variables            -*- no-byte-compile: t -*-
-;;; For more information see (info "(emacs) Directory Variables")
-
-((emacs-lisp-mode . ((flycheck-disabled-checkers . (emacs-lisp-checkdoc)))))
